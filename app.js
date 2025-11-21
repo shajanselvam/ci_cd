dba3dd9b9ba3f9f5fae093c9b6ae3e9fa6ce80d7
shajanselvam <shajan.selvam@gmail.com>
2025-11-21 07:43:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.get('/', (req, res) => {
 <body>
 
   <div class="container">
-    <div class="location">Chennai, India</div>
+    <div class="location">Chennai, tamb India</div>
     <h1>Shajan</h1>
     <p class="subtitle">
       A refined digital space crafted for precision, strategy, and mastery.
@@ -166,6 +166,7 @@ app.get('/health', (req, res) => {
 app.listen(PORT, () => {
   console.log("SNUC Pro Portal running on port " + PORT);
 });
+
 
 
 
